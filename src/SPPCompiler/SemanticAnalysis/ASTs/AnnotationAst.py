5d67d3919ca82dcb91f17fa4080eec6d39d510b9
Sam Gardner <samuelgardner101@gmail.com>
2024-11-19 02:35:28 +0000

    New annotations

--- src/SPPCompiler/SemanticAnalysis/ASTs/AnnotationAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/AnnotationAst.py
@@ -7,8 +7,7 @@ from typing import TYPE_CHECKING
 
 from SPPCompiler.SemanticAnalysis.Meta.Ast import Ast
 from SPPCompiler.SemanticAnalysis.Meta.AstPrinter import ast_printer_method, AstPrinter
-from SPPCompiler.SemanticAnalysis.MultiStage.Stage1_PreProcessor import Stage1_PreProcessor, PreProcessingContext
-from SPPCompiler.SemanticAnalysis.MultiStage.Stage4_SemanticAnalyser import Stage4_SemanticAnalyser
+from SPPCompiler.SemanticAnalysis.MultiStage.Stages import CompilerStages, PreProcessingContext
 
 if TYPE_CHECKING:
     from SPPCompiler.SemanticAnalysis.ASTs.IdentifierAst import IdentifierAst
@@ -32,10 +31,12 @@ class _Annotations(Enum):
     Public = "public"
     Protected = "protected"
     Private = "private"
+    Cold = "cold"
+    Hot = "hot"
 
 
 @dataclass
-class AnnotationAst(Ast, Stage1_PreProcessor, Stage4_SemanticAnalyser):
+class AnnotationAst(Ast, CompilerStages):
     tok_at: TokenAst
     name: IdentifierAst
 
@@ -55,56 +56,68 @@ class AnnotationAst(Ast, Stage1_PreProcessor, Stage4_SemanticAnalyser):
         # Import the necessary classes for type-comparisons to ensure annotation compatibility.
         from SPPCompiler.SemanticAnalysis import FunctionPrototypeAst
         from SPPCompiler.SemanticAnalysis.Mixins.VisibilityEnabled import VisibilityEnabled, AstVisibility
-        from SPPCompiler.SemanticAnalysis.Meta.AstErrors import AstErrors
+        from SPPCompiler.SemanticAnalysis.Errors.SemanticError import SemanticErrors
         super().pre_process(context)
 
         # Pre-process the name of this annotation.
         if self.name.value == _Annotations.VirtualMethod.value:
             # The "virtual_method" annotation can only be applied to functions.
             if not isinstance(context, FunctionPrototypeAst):
-                raise AstErrors.INVALID_ANNOTATION_APPLICATION(self.name, context, "function")
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "function")
             context._virtual = True
 
         elif self.name.value == _Annotations.AbstractMethod.value:
             # The "abstract_method" annotation can only be applied to functions.
             if not isinstance(context, FunctionPrototypeAst):
-                raise AstErrors.INVALID_ANNOTATION_APPLICATION(self.name, context, "function")
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "function")
             context._abstract = context._virtual = True
 
         elif self.name.value == _Annotations.NonImplementedMethod.value:
             # The "non_implemented_method" annotation can only be applied to functions.
             if not isinstance(context, FunctionPrototypeAst):
-                raise AstErrors.INVALID_ANNOTATION_APPLICATION(self.name, context, "function")
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "function")
             context._non_implemented = True
 
         elif self.name.value == _Annotations.Public.value:
             # The "public", access modifier annotation can only be applied to visibility enabled objects.
             if not isinstance(context, VisibilityEnabled):
-                raise AstErrors.INVALID_ANNOTATION_APPLICATION(self.name, context, "visibility-enabled")
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "visibility-enabled")
             context.visibility = AstVisibility.Public
 
         elif self.name.value == _Annotations.Protected.value:
             # The "protected", access modifier annotation can only be applied to visibility enabled objects.
             if not isinstance(context, VisibilityEnabled):
-                raise AstErrors.INVALID_ANNOTATION_APPLICATION(self.name, context, "visibility-enabled")
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "visibility-enabled")
             context.visibility = AstVisibility.Protected
 
         elif self.name.value == _Annotations.Private.value:
             # The "private", access modifier annotation can only be applied to visibility enabled objects.
             if not isinstance(context, VisibilityEnabled):
-                raise AstErrors.INVALID_ANNOTATION_APPLICATION(self.name, context, "visibility-enabled")
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "visibility-enabled")
             context.visibility = AstVisibility.Private
 
+        elif self.name.value == _Annotations.Cold.value:
+            # The "cold" annotation can only be applied to functions.
+            if not isinstance(context, FunctionPrototypeAst):
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "function")
+            context._cold = True
+
+        elif self.name.value == _Annotations.Hot.value:
+            # The "hot" annotation can only be applied to functions.
+            if not isinstance(context, FunctionPrototypeAst):
+                raise SemanticErrors.AnnotationInvalidApplicationError().add(self.name, context, "function")
+            context._hot = True
+
         else:
-            raise AstErrors.UNKNOWN_ANNOTATION(self.name)
+            raise SemanticErrors.AnnotationInvalidError(self.name)
 
     def analyse_semantics(self, scope_manager: ScopeManager, **kwargs) -> None:
-        from SPPCompiler.SemanticAnalysis.Meta.AstErrors import AstErrors
+        from SPPCompiler.SemanticAnalysis.Errors.SemanticError import SemanticErrors
 
         # Prevent duplicate annotations from being applied to an AST.
         annotation_names = self._ctx.annotations.map_attr("name")
         if duplicate_annotations := annotation_names.filter(lambda a: a == self.name).remove(self.name):
-            raise AstErrors.DUPLICATE_ANNOTATION(self.name, duplicate_annotations[0])
+            raise SemanticErrors.AnnotationDuplicateError(self.name, duplicate_annotations[0])
 
 
 __all__ = ["AnnotationAst"]
