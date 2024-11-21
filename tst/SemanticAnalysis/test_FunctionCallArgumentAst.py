25a45b2d017be80715b0bbfe0b6917437170ff20
Sam Gardner <samuelgardner101@gmail.com>
2024-11-21 02:42:27 +0000

    Test suite

---
+++ tst/SemanticAnalysis/test_FunctionCallArgumentAst.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+from tst._Utils import *
+
+
+class TestFunctionCallArgumentAst(TestCase):
+    @should_fail_compilation(SemanticErrors.ExpressionTypeInvalidError)
+    def test_invalid_function_argument_unnamed_expression(self):
+        """
+        fun f(a: std::Bool) -> std::Void { }
+
+        fun g() -> std::Void {
+            f(std::Bool)
+        }
+        """
+
+    @should_fail_compilation(SemanticErrors.ExpressionTypeInvalidError)
+    def test_invalid_function_argument_named_type(self):
+        """
+        fun f(a: std::Bool) -> std::Void { }
+
+        fun g() -> std::Void {
+            f(a=std::Bool)
+        }
+        """
+
+    @should_pass_compilation()
+    def test_valid_function_argument_unnamed_expression(self):
+        """
+        fun f(a: std::Bool) -> std::Void { }
+
+        fun g() -> std::Void {
+            f(true)
+        }
+        """
+
+    @should_pass_compilation()
+    def test_valid_function_argument_named_expression(self):
+        """
+        fun f(a: std::Bool) -> std::Void { }
+
+        fun g() -> std::Void {
+            f(a=true)
+        }
+        """
