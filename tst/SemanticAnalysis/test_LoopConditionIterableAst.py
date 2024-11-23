c6db29af5f540ed2f1bd4efaec07096efa48b04b
Sam Gardner <samuelgardner101@gmail.com>
2024-11-23 05:15:02 +0000

    Test suite

---
+++ tst/SemanticAnalysis/test_LoopConditionIterableAst.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from tst._Utils import *
+
+
+class TestLoopConditionIterableAst(TestCase):
+    @should_fail_compilation(SemanticErrors.ExpressionTypeInvalidError)
+    def test_invalid_loop_condition_iterable_invalid_expression(self):
+        """
+        fun f() -> std::Void {
+            loop x in std::IterMov[std::Str] { }
+        }
+        """
+
+    @should_fail_compilation(SemanticErrors.LoopIterableInvalidTypeError)
+    def test_invalid_loop_condition_iterable_invalid_type(self):
+        """
+        fun f() -> std::Void {
+            loop x in 0 { }
+        }
+        """
+
+    # Todo: this should actually fail because v isn't mutable
+    @should_pass_compilation()
+    def test_valid_loop_condition_iterable(self):
+        """
+        fun f() -> std::Void {
+            let v = std::Vec[std::Str]()
+            loop mut x in v.iter_mut() { }
+        }
+        """
