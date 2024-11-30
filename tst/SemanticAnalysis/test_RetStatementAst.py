a36c4c6cdaaf0e54225088935b2c733bd085fa16
Sam Gardner <samuelgardner101@gmail.com>
2024-11-30 02:46:37 +0000

    Test suite

---
+++ tst/SemanticAnalysis/test_RetStatementAst.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from tst._Utils import *
+
+
+class TestRetStatementAst(TestCase):
+    @should_fail_compilation(SemanticErrors.FunctionCoroutineContainsReturnStatementError)
+    def test_invalid_ret_statement_in_coroutine(self):
+        """
+        cor f() -> std::GenMov[std::BigInt] {
+            ret
+        }
+        """
+
+    @should_fail_compilation(SemanticErrors.TypeMismatchError)
+    def test_invalid_ret_statement_type_mismatch(self):
+        """
+        fun f() -> std::BigInt {
+            ret "hello world"
+        }
+        """
+
+    @should_pass_compilation()
+    def test_valid_ret_statement_1(self):
+        """
+        fun f() -> std::Void {
+            ret
+        }
+        """
+
+    @should_pass_compilation()
+    def test_valid_ret_statement_2(self):
+        """
+        fun f() -> std::BigInt {
+            ret 1
+        }
+        """
