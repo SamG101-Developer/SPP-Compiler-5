e6066f57109c7924272ac1c210d4c3f62a6ad529
Sam Gardner <samuelgardner101@gmail.com>
2024-12-17 19:21:06 +0000

    Convert class to syntactic sugar (TODO WIP)

--- src/SPPCompiler/SemanticAnalysis/ASTs/LoopConditionIterableAst.py
+++ src/SPPCompiler/SemanticAnalysis/ASTs/LoopConditionIterableAst.py
@@ -45,35 +45,39 @@ class LoopConditionIterableAst(Ast, TypeInferrable, CompilerStages):
         from SPPCompiler.SemanticAnalysis import TokenAst, TypeAst
         from SPPCompiler.SemanticAnalysis.Errors.SemanticError import SemanticErrors
         from SPPCompiler.SemanticAnalysis.Meta.AstMutation import AstMutation
-        from SPPCompiler.SemanticAnalysis.Lang.CommonTypes import CommonTypes
+        # from SPPCompiler.SemanticAnalysis.Lang.CommonTypes import CommonTypes
         from SPPCompiler.SyntacticAnalysis.Parser import Parser
 
         # The ".." TokenAst, or TypeAst, cannot be used as an expression for the value.
         if isinstance(self.iterable, (TokenAst, TypeAst)):
             raise SemanticErrors.ExpressionTypeInvalidError().add(self.iterable)
 
-        # Analyse the iterable.
-        self.iterable.analyse_semantics(scope_manager, **kwargs)
-        AstMemoryHandler.enforce_memory_integrity(self.iterable, self.iterable, scope_manager, update_memory_info=False)
+        code = f"{self.iterable}.step() is std::Some(val as {self.variable})"
+        loop_condition_ast = AstMutation.inject_code(code, Parser.parse_loop_expression_condition_boolean)
+        loop_condition_ast.analyse_semantics(scope_manager, **kwargs)
 
-        # Check the iterable is a generator type.
-        iterable_type = self.iterable.infer_type(scope_manager, **kwargs)
-        allowed_types = Seq([CommonTypes.GenMov(), CommonTypes.GenMut(), CommonTypes.GenRef()]).map(TypeAst.without_generics).map(InferredType.from_type)
-        if not allowed_types.any(lambda t: t.symbolic_eq(iterable_type.without_generics(), scope_manager.current_scope)):
-            raise SemanticErrors.LoopIterableInvalidTypeError().add(self.iterable, iterable_type.type)
-
-        # Create a "let" statement to introduce the loop variable into the scope.
-        gen_type = iterable_type.type.types[-1].generic_argument_group["Gen"].value
-        let_ast = AstMutation.inject_code(f"let {self.variable}: {gen_type}", Parser.parse_let_statement_uninitialized)
-        let_ast.analyse_semantics(scope_manager, **kwargs)
-
-        # Set the memory information of the symbol based on the type of iteration.
-        symbols = self.variable.extract_names.map(lambda n: scope_manager.current_scope.get_symbol(n))
-        for symbol in symbols:
-            symbol.memory_info.ast_borrowed = self if iterable_type.type.types[-1].value in ["GenMut", "GenRef"] else None
-            symbol.memory_info.is_borrow_mut = iterable_type.type.types[-1].value == "GenMut"
-            symbol.memory_info.is_borrow_ref = iterable_type.type.types[-1].value == "GenRef"
-            symbol.memory_info.initialized_by(self)
+        # # Analyse the iterable.
+        # self.iterable.analyse_semantics(scope_manager, **kwargs)
+        # AstMemoryHandler.enforce_memory_integrity(self.iterable, self.iterable, scope_manager, update_memory_info=False)
+        #
+        # # Check the iterable is a generator type.
+        # iterable_type = self.iterable.infer_type(scope_manager, **kwargs)
+        # allowed_types = Seq([CommonTypes.GenMov(), CommonTypes.GenMut(), CommonTypes.GenRef()]).map(TypeAst.without_generics).map(InferredType.from_type)
+        # if not allowed_types.any(lambda t: t.symbolic_eq(iterable_type.without_generics(), scope_manager.current_scope)):
+        #     raise SemanticErrors.LoopIterableInvalidTypeError().add(self.iterable, iterable_type.type)
+        #
+        # # Create a "let" statement to introduce the loop variable into the scope.
+        # gen_type = iterable_type.type.types[-1].generic_argument_group["Gen"].value
+        # let_ast = AstMutation.inject_code(f"let {self.variable}: {gen_type}", Parser.parse_let_statement_uninitialized)
+        # let_ast.analyse_semantics(scope_manager, **kwargs)
+        #
+        # # Set the memory information of the symbol based on the type of iteration.
+        # symbols = self.variable.extract_names.map(lambda n: scope_manager.current_scope.get_symbol(n))
+        # for symbol in symbols:
+        #     symbol.memory_info.ast_borrowed = self if iterable_type.type.types[-1].value in ["GenMut", "GenRef"] else None
+        #     symbol.memory_info.is_borrow_mut = iterable_type.type.types[-1].value == "GenMut"
+        #     symbol.memory_info.is_borrow_ref = iterable_type.type.types[-1].value == "GenRef"
+        #     symbol.memory_info.initialized_by(self)
 
 
 __all__ = ["LoopConditionIterableAst"]
