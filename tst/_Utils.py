ede5424291413a6842e092ee82d2a77993dfbd9a
Sam Gardner <samuelgardner101@gmail.com>
2025-02-10 13:59:51 +0000

    Tidy

--- tst/_Utils.py
+++ tst/_Utils.py
@@ -1,41 +1,21 @@
+import os.path
 from argparse import Namespace
 from unittest import TestCase
-import os.path
 
 from SParLex.Parser.ParserError import ParserError
+
 from SPPCompiler.SemanticAnalysis.Errors.SemanticError import SemanticError, SemanticErrors
-from spp_cli import handle_init, handle_build
+from spp_cli import handle_build
 
 
 class CustomTestCase(TestCase):
     ...
 
 
-def _build_temp_project_2(project_dir_name, project_name, code):
-    restore = os.getcwd()
-    fp = f"C:/Users/samue/PycharmProjects/SPP-Compiler-5/tmp/{project_dir_name}/{project_name}"
-
-    try:
-        if not os.path.exists(fp):
-            os.makedirs(os.path.abspath(fp))
-            os.chdir(fp)
-            handle_init()
-            with open("src/main.spp", "a") as f:
-                f.write(code)
-            handle_build(Namespace(mode="rel"))
-
-        else:
-            os.chdir(fp)
-            handle_build(Namespace(mode="rel"))
-
-    finally:
-        os.chdir(restore)
-
-
 def _build_temp_project_v3(code):
     cwd = os.getcwd()
     fp = f"C:/Users/samue/PycharmProjects/SPP-Compiler-5/tst/test_outputs"
-    with open(f"{fp}/src/test_outputs/main.spp", "w") as f:
+    with open(f"{fp}/src/main.spp", "w") as f:
         f.write(code)
     os.chdir(fp)
     handle_build(Namespace(mode="rel"))
