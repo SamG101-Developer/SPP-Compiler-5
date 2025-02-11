7dcb25b809fd7b6a70559f56dce074a794f70b68
Sam Gardner <samuelgardner101@gmail.com>
2025-02-11 01:46:39 +0000

    Test suite

---
+++ tst/SemanticAnalysis/ASTs/test_ClassPrototypeAst.py
@@ -0,0 +1,35 @@
+from tst._Utils import *
+
+
+class TestClassPrototypeAst(CustomTestCase):
+    @should_fail_compilation(SemanticErrors.RecursiveTypeDefinitionError)
+    def test_invalid_recursive_class_prototype(self):
+        """
+        cls A {
+            a: A
+        }
+        """
+
+    @should_fail_compilation(SemanticErrors.RecursiveTypeDefinitionError)
+    def test_invalid_recursive_class_prototype_complex(self):
+        """
+        cls A {
+            a: B
+        }
+
+        cls B {
+            a: A
+        }
+        """
+
+    @should_pass_compilation()
+    def test_valid_class_prototype_definition(self):
+        """
+        cls A {
+            a: B
+        }
+
+        cls B {
+            a: std::Str
+        }
+        """
