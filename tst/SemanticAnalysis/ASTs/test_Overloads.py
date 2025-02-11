51e49a5978cf715d9b216ba4417df92b72b12ea9
Sam Gardner <samuelgardner101@gmail.com>
2025-02-11 01:23:29 +0000

    Test suite

--- tst/SemanticAnalysis/ASTs/test_Overloads.py
+++ tst/SemanticAnalysis/ASTs/test_Overloads.py
@@ -25,3 +25,25 @@ class TestOverloads(CustomTestCase):
             b.f(true, 1)
         }
         """
+
+    @should_fail_compilation(SemanticErrors.FunctionCallNoValidSignaturesError)
+    def test_invalid_override_call(self):
+        """
+        cls A { }
+        cls B { }
+
+        sup A {
+            @virtual_method fun f(&self) -> std::Void { }
+            @virtual_method fun f(&self, a: A) -> std::Void { }
+            @virtual_method fun f(&self, a: std::Bool, b: std::BigInt) -> std::Void { }
+        }
+
+        sup B ext A {
+            fun f(&self) -> std::Void { }
+        }
+
+        fun test() -> std::Void {
+            let b = B()
+            b.f("a")
+        }
+        """
