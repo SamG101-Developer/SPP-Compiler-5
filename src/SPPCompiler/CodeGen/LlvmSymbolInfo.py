c6e0ad052d5088d470db5d033c16f6de7163bcbe
Sam Gardner <samuelgardner101@gmail.com>
2025-04-25 04:05:27 +0100

    Remove llvm generation for now

--- src/SPPCompiler/CodeGen/LlvmSymbolInfo.py
+++ src/SPPCompiler/CodeGen/LlvmSymbolInfo.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass, field
-from llvmlite import ir
+# from llvmlite import ir
 from typing import Optional
 
 
-@dataclass(kw_only=True)
+# @dataclass(slots=True)(kw_only=True)
 class LlvmSymbolInfo:
-    llvm_type: Optional[ir.IdentifiedStructType] = field(default=None)
-    llvm_module: ir.Module = field(default=None)
+    ...
+    # llvm_type: Optional[ir.IdentifiedStructType] = field(default=None)
+    # llvm_module: ir.Module = field(default=None)
 
 
 __all__ = ["LlvmSymbolInfo"]
