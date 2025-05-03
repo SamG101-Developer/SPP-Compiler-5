4654962df8e248187bcd493dfc571c20b2eab1bf
Sam Gardner <samuelgardner101@gmail.com>
2025-05-03 03:44:40 +0100

    Bug fix for function ownership testing

--- src/SPPCompiler/SemanticAnalysis/AstUtils/AstFunctionUtils.py
+++ src/SPPCompiler/SemanticAnalysis/AstUtils/AstFunctionUtils.py
@@ -54,14 +54,14 @@ class AstFunctionUtils:
             function_name = lhs.op.field
             function_owner_scope = sm.current_scope.get_symbol(function_owner_type).scope
 
-        # Static access into a type: "Type::method()".
+        # Static access into a type: "Type::method()" or "ns::Type::method()".
         elif isinstance(lhs, Asts.PostfixExpressionAst) and isinstance(lhs.lhs, Asts.TypeAst) and lhs.op.is_static_access():
             function_owner_type = lhs.lhs
             function_name = lhs.op.field
             function_owner_scope = sm.current_scope.get_symbol(function_owner_type).scope
 
         # Direct access into a namespaced free function: "std::console::print("hello")".
-        elif isinstance(lhs, Asts.PostfixExpressionAst) and isinstance(lhs.lhs.op.field, Asts.IdentifierAst) and lhs.op.is_static_access():
+        elif isinstance(lhs, Asts.PostfixExpressionAst) and lhs.op.is_static_access():
             function_owner_scope = sm.current_scope.get_namespace_symbol(lhs.lhs).scope
             function_name = lhs.op.field
             function_owner_type = function_owner_scope.get_symbol(function_name).type
