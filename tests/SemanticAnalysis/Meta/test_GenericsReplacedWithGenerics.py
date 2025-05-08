d63f794940b66dfe0009b37ee5fa4430a1172f43
Sam Gardner <samuelgardner101@gmail.com>
2025-05-08 05:11:27 +0100

    Test suite

--- tests/SemanticAnalysis/Meta/test_GenericsReplacedWithGenerics.py
+++ tests/SemanticAnalysis/Meta/test_GenericsReplacedWithGenerics.py
@@ -5,9 +5,16 @@ class TestGenericsReplacedWithGenerics(CustomTestCase):
     @should_pass_compilation()
     def test_generic_replaced_with_generic(self):
         """
+        use std::string::Str
         use std::vector::Vec
+
         fun g[U]() -> Vec[U] {
             ret Vec[U]()
+        }
+
+        fun f() -> std::void::Void {
+            let mut x = g[Str]()
+            x.push(element="hello")
         }
         """
 
@@ -16,8 +23,14 @@ class TestGenericsReplacedWithGenerics(CustomTestCase):
         """
         use std::option::Opt
         use std::option::Some
+        use std::string::Str
 
         fun g[U]() -> Opt[U] {
             ret Some[U]()
+        }
+
+        fun f() -> std::void::Void {
+            let mut x = g[Str]()
+            x = Some(val="hello")
         }
         """
