7d04d178d884c3c6e11e210c993654452d7452ce
Sam Gardner <samuelgardner101@gmail.com>
2025-05-21 14:35:21 +0100

    Test suite

--- tests/SemanticAnalysis/Meta/test_Specialization.py
+++ tests/SemanticAnalysis/Meta/test_Specialization.py
@@ -29,7 +29,7 @@ class TestSpecialization(CustomTestCase):
         """
 
     @should_fail_compilation(SemanticErrors.IdentifierUnknownError)
-    def test_specialization_vector_bool_for_vector_string(self) -> None:
+    def test_specialization_failure_different_generic(self) -> None:
         """
         sup std::vector::Vec[std::string::Str] {
             fun test_func(&self) -> std::number::bigint::BigInt {
@@ -42,8 +42,43 @@ class TestSpecialization(CustomTestCase):
 
         fun f() -> std::void::Void {
             let v = Vec[Bool]()
-            let mut x = v.test_func()
-            x = 1234
+            let x = v.test_func()
+        }
+        """
+
+    @should_pass_compilation()
+    def test_methods_on_different_generic_names(self) -> None:
+        """
+        cls MyType[T] {
+            a: T
+        }
+
+        sup [T] MyType[T] {
+            fun test_func_0(self) -> T {
+                ret self.a
+            }
+        }
+
+        sup [U] MyType[U] {
+            fun test_func_1(self) -> U {
+                ret self.a
+            }
+        }
+
+        sup std::number::bigint::BigInt {
+            fun to_string(&self) -> std::string::Str {
+                ret ""
+            }
+        }
+
+        fun f() -> std::void::Void {
+            let t = MyType[std::number::bigint::BigInt]()
+            let a = t.test_func_0()
+
+            let u = MyType[std::string::Str]()
+            let mut b = u.test_func_1()
+
+            b = a.to_string()
         }
         """
 
