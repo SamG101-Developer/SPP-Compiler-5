058a8238835a8360e75bc2fed94cc56713a61c6a
Sam Gardner <samuelgardner101@gmail.com>
2025-07-22 18:59:10 +0100

    Add missing memory checks for the `iter` variable branch

--- src/SPPCompiler/SemanticAnalysis/Asts/IterPatternVariableAst.py
+++ src/SPPCompiler/SemanticAnalysis/Asts/IterPatternVariableAst.py
@@ -37,20 +37,25 @@ class IterPatternVariableAst(Asts.Ast, Asts.Mixins.AbstractPatternVariantAst):
     def analyse_semantics(self, sm: ScopeManager, cond: Asts.ExpressionAst = None, **kwargs) -> None:
         # Create a dummy type with the same type as the variable's type, to initialize it. This has to be done, to use
         # the "Yield" type, as "cond" is the "Generated" type.
-        dummy_type = cond.infer_type(sm, **kwargs).type_parts[-1].generic_argument_group["Yield"].value
-        dummy = Asts.ObjectInitializerAst(class_type=dummy_type)
+        var_type = cond.infer_type(sm, **kwargs).type_parts[-1].generic_argument_group["Yield"].value
 
         # Create a new AST node that initializes the variable with the dummy value.
-        self._new_ast = Asts.LetStatementInitializedAst(self.pos, assign_to=self.variable, value=dummy)
+        self._new_ast = Asts.LetStatementInitializedAst(self.pos, explicit_type=var_type, assign_to=self.variable, value=Asts.ObjectInitializerAst(pos=self.pos, class_type=var_type))
         self._new_ast.analyse_semantics(sm, **kwargs)
 
         # If the "Yield" type is a borrow, set the borrow flags in the memory info of the variable.
-        # conv = self._new_ast.value.infer_type(sm, **kwargs).convention
-        # for name in self.variable.extract_names:
-        #     sym = sm.current_scope.get_symbol(name)
-        #     sym.memory_info.ast_borrowed = conv
-        #     sym.memory_info.is_borrow_mut = isinstance(conv, Asts.ConventionMutAst)
-        #     sym.memory_info.is_borrow_ref = isinstance(conv, Asts.ConventionRefAst)
+        conv = var_type.convention
+        for name in self.variable.extract_names:
+            sym = sm.current_scope.get_symbol(name)
+            sym.memory_info.initialized_by(self.variable)
+
+            # Apply the borrow to the symbol.
+            sym.memory_info.ast_borrowed = conv
+            sym.memory_info.is_borrow_mut = isinstance(conv, Asts.ConventionMutAst)
+            sym.memory_info.is_borrow_ref = isinstance(conv, Asts.ConventionRefAst)
+
+            # Apply the borrow to the type.
+            sym.type = sym.type.with_convention(conv)
 
     def check_memory(self, sm: ScopeManager, **kwargs) -> None:
         self._new_ast.check_memory(sm, **kwargs)
