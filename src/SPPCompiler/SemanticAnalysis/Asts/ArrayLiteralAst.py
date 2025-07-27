f01b5ca2561a4aef9c43217936d73075c58960a4
Sam Gardner <samuelgardner101@gmail.com>
2025-07-27 17:22:35 +0100

    New arrays

--- src/SPPCompiler/SemanticAnalysis/Asts/ArrayLiteralAst.py
+++ src/SPPCompiler/SemanticAnalysis/Asts/ArrayLiteralAst.py
@@ -5,8 +5,8 @@ from typing import Union
 from SPPCompiler.SemanticAnalysis import Asts
 
 ArrayLiteralAst = Union[
-    Asts.ArrayLiteral0ElementAst,
-    Asts.ArrayLiteralNElementAst]
+    Asts.ArrayLiteralRepeatedElementAst,
+    Asts.ArrayLiteralExplicitElementsAst]
 """An ArrayLiteralAst is either a 0-element (empty) or n-element (filled) array."""
 
 __all__ = [
